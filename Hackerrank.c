aa15ddefd540fb3f1283e0f317d8431fd44ea394
Drona Srivastava <srivastavadrona@gmail.com>
2024-04-30 12:47:59 +0530

    Random hackerrank question

---
+++ Hackerrank.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num, *arr, i;
+    scanf("%d", &num);
+    arr = (int*) malloc(num * sizeof(int));
+    for(i = 0; i < num; i++) {
+        scanf("%d", &arr[i]);
+    }
+    for(int i=0, j=(num-1); i<num ,j>0; i++,j--){
+        arr[i] = arr[j];
+    }
+    for(int i=0; i<num ;i++){
+        printf("%d",*(arr+i));
+    }
+}
